3bd84704ba86a70c34821d9fc1b07b2eae7ab700
nomsh <Onome@users.noreply.github.com>
2020-01-16 19:53:17 -0600

    Opened my heart.

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ print("hello, I'm main.py")
 for i in range(20):
     print(i)
 
-    
+print("This is annother print statement") 
+print("This class is moving fast today, Love!")  
+print("We definitely needed this")
